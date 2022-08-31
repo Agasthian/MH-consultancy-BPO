b39ec48f5d57c4d41132fc176283812c2c662410
Agasthian <agasthian_r@hotmail.com>
2022-08-31 21:39:40 +0530

    Added Source code for the site

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Mobile Menu
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', ()=>{
+    navUL.classList.toggle('show');
+})
+
+// Animate on Scroll - library - config
+AOS.init();
+
+//Swiper js
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
